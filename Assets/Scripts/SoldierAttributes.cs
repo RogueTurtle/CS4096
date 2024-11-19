6fcef6fd63f3980158bb8a1314e0b93916e795b2
RogueTurtle <66068126+RogueTurtle@users.noreply.github.com>
2024-11-19 20:58:06 +0000

    added soldier attributes script

---
+++ Assets/Scripts/SoldierAttributes.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class SoldierAttributes : MonoBehaviour
+{
+    public float health = 10f;
+    public float damage = 3f;
+    public float speed = 10f;
+    public float range = 50f;
+}
